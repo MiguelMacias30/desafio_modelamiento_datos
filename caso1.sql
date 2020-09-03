8e83c74fd1c03fcbb58632b9ba279400700f9b96
Miguel Macias <miguelfernandomacias@gmail.com>
2020-09-02 23:57:56 -0400

    descripcion

---
+++ caso1.sql
@@ -0,0 +1,6 @@
+--caso 1
+--Un trabajador pertenece a un departamento.
+--El trabajador tiene un nombre, rut y dirección.
+--El departamento tiene un nombre.
+--Un trabajador posee liquidaciones.
+--La liquidación se guarda en un archivo drive
